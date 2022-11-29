8f35b539ff0b4f4db0c5bae691216a6ba9c9d232
Mohammad Turaab <mohammadturaab@Mohammads-MacBook-Pro.local>
2022-11-29 14:48:38 -0500

    added rollbar

--- app.go
+++ app.go
@@ -3,20 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/rollbar/rollbar-go"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my webiste</h1>")
-
-	fmt.Printf("<h1>Enter your name: </h1>")
-	var name string
-	fmt.Scan(&name)
-
-	fmt.Printf("<h2>Hello, %v</h2>", name)
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
+	rollbar.SetToken("db0eeaf619584284873f3ba1fbc012e1")
+	rollbar.SetEnvironment("production")
+
+	rollbar.Info("Message body goes here")
+	rollbar.WrapAndWait(doSomething)
+}
+
+func doSomething() {
+	var timer *time.Timer = nil
+	timer.Reset(10) // this will panic
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module rollbar/rollbar-go
+
+go 1.18
+
+require github.com/rollbar/rollbar-go v1.4.5
