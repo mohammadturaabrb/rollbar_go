5f56078326c79ee4e2c27c630a62bd60991870aa
Mohammad Turaab <mohammadturaab@Mohammads-MacBook-Pro.local>
2022-12-14 12:23:11 -0600

    adding login and signup function

--- app.go
+++ app.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/rollbar/rollbar-go"
+	"github.com/gorilla/mux"
 )
 
 // func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,10 +27,56 @@ func main() {
 	http.HandleFunc("/feeling", feelingHandler)
 	http.ListenAndServe(":3000", nil)
 
+	router := mux.NewRouter()
+	router := ("/signup", signup).Methods("POST")
+
+
 	rollbar.SetToken("db0eeaf619584284873f3ba1fbc012e1")
 	rollbar.SetEnvironment("production")
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	// Get the signup data from the request body
+	decoder := json.NewDecoder(r.Body)
+	var userData UserData
+	err := decoder.Decode(&userData)
+	if err != nil {
+	  // Return an error response if the signup data is invalid
+	  return
+	}
+  
+	// Validate the signup data
+	if userData.Username == "" || userData.Password == "" {
+	  // Return an error response if the signup data is invalid
+	  return
+	}
+  
+	// Hash the password for security
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userData.Password), bcrypt.DefaultCost)
+	if err != nil {
+	  // Return an error response if there is a problem with the password
+	  return
+	}
+  
+	// Store the signup data in a database
+	db, err := sql.Open("mysql", "user:password@/database")
+	if err != nil {
+	  // Return an error response if there is a problem with the database
+	  return
+	}
+	defer db.Close()
+  
+	stmt, err := db.Prepare("INSERT INTO users(username, password) VALUES(?, ?)")
+	if err != nil {
+	  // Return an error response if there is a problem with the database
+	  return
+	}
+	defer stmt.Close()
+  
+	_, err = stmt.Exec(userData.Username, hashedPassword)
+	if err != nil {
+	  // Return an error response if there is a problem with the database
+  
 type Feeling struct {
 	Name        string
 	Description string
@@ -63,6 +110,8 @@ var feelings []Feeling = []Feeling{
 		Filename:    "surprised.jpg",
 	},
 }
+
+
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// parse the HTML template
